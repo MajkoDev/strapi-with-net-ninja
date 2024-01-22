75d018c62c9ba3c60248246290286c388c03aafc
Marian <hellomariandev@gmail.com>
2024-01-22 17:50:37 +0100

    query all reviews

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,12 +1,44 @@
+import { Link } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
 
-import { Link } from 'react-router-dom'
-import { useQuery, gql } from '@apollo/client'
+const REVIEWS = gql`
+  query GetReviews {
+    reviews {
+      data {
+        attributes {
+          title
+          rating
+          body
+        }
+        id
+      }
+    }
+  }
+`;
 
 export default function Homepage() {
+  const { loading, error, data } = useQuery(REVIEWS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :</p>
+
   return (
     <div>
       <h1>Homepage</h1>
-      
+
+      {data.reviews.data.map((review) => (
+        <div key={review.id} className="review-card">
+          <div className="rating">{review.attributes.rating}</div>
+          <h2>{review.attributes.title}</h2>
+
+          <small>console list</small>
+
+          <p>
+            {review.attributes.body[0].children[0].text.substring(0, 200)}...
+          </p>
+          <Link to={`/details/${review.id}`}>Read more</Link>
+        </div>
+      ))}
     </div>
-  )
+  );
 }
