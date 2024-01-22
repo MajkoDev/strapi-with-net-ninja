0ff5946eb253aa7ad9cffddec79571e6c560b8fa
Marian <hellomariandev@gmail.com>
2024-01-22 17:50:27 +0100

    query variables

--- frontend/src/pages/ReviewDetails.jsx
+++ frontend/src/pages/ReviewDetails.jsx
@@ -1,20 +1,36 @@
 import { useParams } from "react-router-dom";
+import { useQuery, gql } from "@apollo/client";
+import { BlocksRenderer } from "@strapi/blocks-react-renderer";
+
+const REVIEW = gql`
+  query GetReview($id: ID!) {
+    review(id: $id) {
+      data {
+        id
+        attributes {
+          title
+          rating
+          body
+        }
+      }
+    }
+  }
+`;
 
 export default function ReviewDetails() {
-  let data = {
-    rating: 3,
-    title: "Jojo",
-    body: "Lorem ipsum",
-  };
+  const { id } = useParams();
+  const { loading, error, data } = useQuery(REVIEW, {
+    variables: { id: id },
+  });
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
 
   return (
     <div className="review-card">
-      <div className="rating">{data.rating}</div>
-      <h2>{data.title}</h2>
-
-      <small>console list</small>
-
-      <p>{data.body}</p>
+      <div className="rating">{data.review.data.attributes.rating}</div>
+      <h2>{data.review.data.attributes.title}</h2>
+      <BlocksRenderer content={data.review.data.attributes.body} />
     </div>
   );
 }
